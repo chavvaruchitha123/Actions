8f9dd7efeccf5a7915b40808e17f8d4f7a13617a
chavvaruchitha123 <150325332+chavvaruchitha123@users.noreply.github.com>
2024-07-26 16:26:17 +0530

    Update ecosia.js

--- test/ecosia.js
+++ test/ecosia.js
@@ -3,18 +3,15 @@ describe('Ecosia.org Demo', function() {
 
   it('Demo test ecosia.org', function(browser) {
     browser
-      .waitForElementVisible('body', 10000) // Increased timeout to 10 seconds
-      .getTitle(function(title) {
-        console.log('Page title is: ' + title); // Log the page title for debugging
-      })
-      
-      
-      .setValue('input[type=search]', 'nightwatch')
-      
-      .click('button[type=submit]')
+      .waitForElementVisible('body')
     
-     
+      .assert.visible('input[type=search]')
+      .setValue('input[type=search]', 'nightwatch')
+      .assert.visible('button[type=submit]')
+      .click('button[type=submit]')
+      
   });
 
   after(browser => browser.end());
 });
+
